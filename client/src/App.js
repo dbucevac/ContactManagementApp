fe91abfef353fd1dd338430d1fec9f10f18b97ac
Darko B <darkobuchevac@gmail.com>
2020-10-12 21:36:50 +0200

    adding external libraries + including bootstrap in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+       <h1>Hello</h1> 
       </header>
     </div>
   );
